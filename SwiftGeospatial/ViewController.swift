90122f01dc56138574eb3b6169a4117c6a779f87
sinano1107 <45984123+sinano1107@users.noreply.github.com>
2022-12-01 13:45:57 +0900

    ライセンスと改変の内容を明記

--- SwiftGeospatial/ViewController.swift
+++ SwiftGeospatial/ViewController.swift
@@ -1,9 +1,26 @@
-//
-//  ViewController.swift
-//  SwiftGeospatial
-//
-//  Created by 長政輝 on 2022/11/27.
-//
+/*
+ * ==========  Details of modification  ==========
+ * This file is my rewrite of the source code implemented in objective-c to Swift.
+ * The original file is available here.
+ *
+ *      https://github.com/google-ar/arcore-ios-sdk/blob/v1.34.0/Examples/GeospatialExample/GeospatialExample/ViewController.m
+ * 
+ * ===============================================
+ * 
+ * Copyright 2021 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import UIKit
 import ARKit
